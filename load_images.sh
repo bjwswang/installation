93e41d5cd4c70e3a9eb1bf55d5448309e66c627a
bjwswang <bjwswang@gmail.com>
2024-04-12 14:27:57 +0800

    scripts to load and save images
    Signed-off-by: bjwswang <bjwswang@gmail.com>

--- load_images.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Get the list of images from 'docker images' command
-image_list=$(docker images --format "{{.Repository}}:{{.Tag}}")
-
-# Load images into Kind cluster
-for image in $image_list; do
-    kind load docker-image "$image" --name kubeagi
-done
---
+++ scripts/prepare_charts.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# 读取 JSON 文件
+json=$(cat charts.json)
+
+# 解析 JSON 数据并克隆代码库
+for row in $(echo "$json" | jq -r '.[] | @base64'); do
+    _jq() {
+        echo ${row} | base64 --decode | jq -r ${1}
+    }
+
+    repo_name=$(_jq '.name')
+    repo_url=$(_jq '.url')
+
+    echo "Cloning $repo_name from $
